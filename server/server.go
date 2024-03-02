fc782cfd713626ca7e082029f76f898a6769dc7c
Tarun Nahak <tarunnahak25@gmail.com>
2024-03-02 12:22:33 +0530

    added server with test

--- server/server.go
+++ server/server.go
@@ -1,2 +1,67 @@
 // Package server contains everything for setting up and running the HTTP server.
 package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+// New will create the new instance of servers overtime
+func New(opts Options) *Server {
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+
+	mux := chi.NewMux()
+	return &Server{
+		address: address,
+		mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
+}
+
+// Start the server by setting up routes and listening for http request on the given address
+func (s *Server) Start() error {
+	s.setupRoutes()
+
+	fmt.Println("Starting on", s.address)
+	if err := s.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("error starting server: %w", err)
+	}
+	return nil
+}
+
+// Stop the server gracefully within the timeout
+func (s *Server) Stop() error {
+	fmt.Println("Stopping")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error stopping server: %w", err)
+	}
+	return nil
+}
